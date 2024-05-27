8e4a00fd523a54d88adec33b8f66b1fa4657ce1c
yvirdi <yvirdi@uwo.ca>
2024-05-27 16:40:30 -0400

    Initial Commit

---
+++ monitor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  monitor
+//
+//  Created by Yuvraj Virdi on 2024-05-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ monitor/monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  monitorApp.swift
+//  monitor
+//
+//  Created by Yuvraj Virdi on 2024-05-27.
+//
+
+import SwiftUI
+
+@main
+struct monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
