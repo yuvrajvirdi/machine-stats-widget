85c83406b30419fa07b1e68a18234ab6eec808c9
yvirdi <yvirdi@uwo.ca>
2024-05-27 19:26:04 -0400

    commit

---
+++ monitor/CPUMonitor.swift
@@ -0,0 +1,8 @@
+//
+//  CPUMonitor.swift
+//  monitor
+//
+//  Created by Yuvraj Virdi on 2024-05-27.
+//
+
+import Foundation
---
+++ monitor/StatsMonitor.swift
@@ -0,0 +1,8 @@
+//
+//  StatsMonitor.swift
+//  monitor
+//
+//  Created by Yuvraj Virdi on 2024-05-27.
+//
+
+import Foundation
